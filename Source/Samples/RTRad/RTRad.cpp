05ac04eaeabc13590af9ffb6492009ebed5d3fdd
Benjamin Kahl <benjaminkahl19@gmail.com>
2021-10-28 22:59:53 +0200

    Moved main to own file

--- Source/Samples/RTRad/RTRad.cpp
+++ Source/Samples/RTRad/RTRad.cpp
@@ -73,13 +73,3 @@ void RTRad::onHotReload(HotReloadFlags reloaded)
 void RTRad::onResizeSwapChain(uint32_t width, uint32_t height)
 {
 }
-
-int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
-{
-    RTRad::UniquePtr pRenderer = std::make_unique<RTRad>();
-    SampleConfig config;
-    config.windowDesc.title = "Falcor Project Template";
-    config.windowDesc.resizableWindow = true;
-    Sample::run(config, pRenderer);
-    return 0;
-}
